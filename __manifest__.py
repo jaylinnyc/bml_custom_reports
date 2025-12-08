e19f71f1d6231b46a7681a052ace255308702029
Jay <jaylin8@gmail.com>
2025-12-08 16:17:14 +0700

    tax invoice product description

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,7 @@
         'security/ir.model.access.csv',
         'views/report_footer_config_views.xml',
         'views/report_purchase_order.xml',   # keep your footer override
+        'views/report_invoice.xml',          # tax invoice with product description
         'views/report_inventory_operations.xml',  # inventory repair & consumption reports
         'views/account_move_views.xml',      # bill date sync feature
         'data/report_footer_data.xml',       # default footer configurations
