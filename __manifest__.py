7dff0b45a83e865e55317955b9dcb8590eaad985
Jay <jaylin8@gmail.com>
2026-01-05 21:44:07 +0800

    feat: add Auto Reconcile button directly to bank reconciliation widget header

--- __manifest__.py
+++ __manifest__.py
@@ -16,6 +16,12 @@
         'views/account_bank_statement_views.xml',  # manual auto-reconcile action
         'data/report_footer_data.xml',       # default footer configurations
     ],
+    'assets': {
+        'web.assets_backend': [
+            'bml_custom_reports/static/src/js/bank_rec_auto_reconcile.js',
+            'bml_custom_reports/static/src/xml/bank_rec_auto_reconcile.xml',
+        ],
+    },
     'installable': True,
     'application': False,
     'auto_install': False,
