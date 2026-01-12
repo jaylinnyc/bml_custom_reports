f9d49db812077001219b5ae7887dfc877696eca7
Jay <jaylin8@gmail.com>
2026-01-12 16:44:35 +0800

    fix button again

--- static/src/js/thai_bank_upload_button.js
+++ static/src/js/thai_bank_upload_button.js
@@ -33,7 +33,6 @@ export class BankStatementListController extends ListController {
 export const bankStatementListView = {
     ...listView,
     Controller: BankStatementListController,
-    buttonTemplate: "bml_custom_reports.ThaiBankUploadButton",
 };
 
 // Register the custom view for bank statements list
